301d69ad740517d6a87d66135a4e852af3db62de
Marcos Monzon <marcos.monzon@intraway.com>
2024-11-19 10:29:07 -0300

    Clase 11/13 Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,10 @@
+// var naemOfDog; // undefined
+
+// console.log(naemOfDog);
+// var naemOfDog = "Elmo";
+// console.log(naemOfDog);
+nameOfDog();
+function nameOfDog() {
+  console.log(elmo);
+}
+var elmo = "dog"; // lo eleva como var elmo = undefined
